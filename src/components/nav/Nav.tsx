824f9af4b2f00983d1efc286ab82a9bdf2a7c2fe
ananddharne <anand.dharne24@gmail.com>
2026-04-05 18:52:10 -0400

    fix: update nav CTA label

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -140,7 +140,7 @@ export function Nav() {
               letterSpacing: '0.06em',
             }}
           >
-            Hire me
+            Contact me
           </a>
         </div>
       )}
